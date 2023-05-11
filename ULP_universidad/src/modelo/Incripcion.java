eff468fd81bde6130604ea86e9e88c89c3bccbab
viblasco <viviblasco5d@gmail.com>
2023-05-11 11:26:19 -0300

    subida clases materia y inscripto

--- ULP_universidad/src/modelo/Incripcion.java
+++ ULP_universidad/src/modelo/Incripcion.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author VIB
- */
 public class Incripcion {
-    
+  private int id_incripto;
+  private double nota;
+  private Alumno alumno;
+  private Materia materia;
+
+    public Incripcion() {
+    }
+
+    public Incripcion(int id_incripto, double nota, Alumno alumno, Materia materia) {
+        this.id_incripto = id_incripto;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Incripcion(double nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public int getId_incripto() {
+        return id_incripto;
+    }
+
+    public void setId_incripto(int id_incripto) {
+        this.id_incripto = id_incripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        return "Incripcion{" + "id_incripto=" + id_incripto + ", nota=" + nota + ", alumno=" + alumno + ", materia=" + materia + '}';
+    }
+ 
+  
 }
--- ULP_universidad/src/modelo/Materia.java
+++ ULP_universidad/src/modelo/Materia.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author VIB
- */
 public class Materia {
+    private int id_materia;
+    private String nombre;
+    private int anio;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int anio, boolean estado) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public Materia(int id_materia, String nombre, int anio, boolean estado) {
+        this.id_materia = id_materia;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public int getId_materia() {
+        return id_materia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setId_materia(int id_materia) {
+        this.id_materia = id_materia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "id_materia=" + id_materia + ", nombre=" + nombre + ", anio=" + anio + ", estado=" + estado + '}';
+    }
+    
     
 }
