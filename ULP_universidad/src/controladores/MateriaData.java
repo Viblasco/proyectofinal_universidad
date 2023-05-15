fe9e1c899b13688f365e7148a83a2b244f4e0015
GiuliBruno14 <128742172+GiuliBruno14@users.noreply.github.com>
2023-05-15 15:59:57 -0300

    MateriaData

--- ULP_universidad/src/controladores/MateriaData.java
+++ ULP_universidad/src/controladores/MateriaData.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controladores;
 
-/**
- *
- * @author VIB
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Materia;
+
 public class MateriaData {
+    private Connection con;
+
+    public MateriaData() {
+        con = Conexion.getConexion();
+    }
+    
+    public void guardarMateria(Materia materia){
+        String sql =  "INSERT INTO materia(nombre,anio,estado)VALUES (?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,materia.getNombre());
+           ps.setInt(2, materia.getAnio());
+           ps.setBoolean(3, materia.isEstado());
+           ps.executeUpdate();
+           ResultSet rs= ps.getGeneratedKeys();
+           if (rs.next()){
+               materia.setId_materia(rs.getInt(1));
+           } else {
+               System.out.println("La materia no se pudo guardar");
+           }
+           ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void actualizarMateria(Materia materia){
+        String sql = "UPDATE materia SET nombre=?,anio=?,estado=? WHERE id_materia=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.setInt(4, materia.getId_materia());
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Materia buscarMateria(int id){
+        Materia mat = null;
+        String sql = "SELECT id_materia, nombre, anio, estado FROM materia WHERE id_materia=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                mat = new Materia();
+                mat.setId_materia(rs.getInt("id_materia"));
+                mat.setNombre(rs.getString("nombre"));
+                mat.setAnio(rs.getInt("anio"));
+                mat.setEstado(rs.getBoolean("estado"));
+            } else {
+                System.out.println("Materia inexistente");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return mat;
+    }
+    
     
 }
--- ULP_universidad/src/ulp_universidad/ULP_universidad.java
+++ ULP_universidad/src/ulp_universidad/ULP_universidad.java
@@ -2,19 +2,28 @@
 package ulp_universidad;
 
 import controladores.AlumnoData;
+import controladores.MateriaData;
 import java.time.LocalDate;
 import java.time.Month;
 import modelo.Alumno;
+import modelo.Materia;
 
 public class ULP_universidad {
 
     public static void main(String[] args) {
        //Alumno alu = new Alumno("Suarez","Mariana",444444,LocalDate.of(2000, 2, 20),true);
-       Alumno alu2 = new Alumno(5,"Suarez","Mariana",555555,LocalDate.of(2000, 2, 20),true);
-       AlumnoData ad = new AlumnoData();
+       //Alumno alu2 = new Alumno(5,"Suarez","Mariana",555555,LocalDate.of(2000, 2, 20),true);
+       //AlumnoData ad = new AlumnoData();
        //ad.guardarAlumno(alu);
        //ad.actualizarAlumno(alu2);
-        System.out.println("El alumno es"+ad.buscarAlumno(5));
+       //System.out.println("El alumno es"+ad.buscarAlumno(5));
+       
+//       Materia mat = new Materia("Matematica", 1,true);
+       Materia mat1 = new Materia(6,"Matematica",2,true);
+       MateriaData an = new MateriaData();
+       //an.guardarMateria(mat);
+       //an.actualizarMateria(mat1);
+        System.out.println("La materia es"+an.buscarMateria(5));
     }
     
 }
