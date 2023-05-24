87a9fdb66df288e8e63eb35a9cbcad1d02049566
Viblasco <viviblasco5d@gmail.com>
2023-05-23 22:11:53 -0300

    hecho por Daniel

--- ULP_universidad/src/controladores/InscripcionData.java
+++ ULP_universidad/src/controladores/InscripcionData.java
@@ -27,7 +27,7 @@ public class InscripcionData {
         String sql = "INSERT INTO inscripcion(nota, id_alumno, id_materia) VALUES (?,?,?);";
         try {
             PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            ps.setDouble(1,inscripcion.getNota()); //CAMBIAR A DOUBLE EN BASE DE DATOS!!!!!
+            ps.setDouble(1,inscripcion.getNota()); 
             ps.setInt(2,inscripcion.getAlumno().getId());
             ps.setInt(3,inscripcion.getMateria().getId_materia());
             ps.executeUpdate();
@@ -70,6 +70,131 @@ public class InscripcionData {
         }
         return listaInscriptos;
         
+    }
+    public List<Inscripcion> obtenerInscripcionXAlumno(int id_alumno) {
+        List<Inscripcion> lista = new ArrayList<>();
+        Inscripcion cursada;
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM inscripcion WHERE id_alumno = ?", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id_alumno);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                cursada = new Inscripcion();
+                cursada.setId_inscripto(rs.getInt("id_inscripto"));
+                
+                Alumno a=aluData.buscarAlumno(rs.getInt("id_alumno"));
+                cursada.setAlumno(a);
+                
+                Materia m=matData.buscarMateria(rs.getInt("id_materia"));
+                cursada.setMateria(m);
+                cursada.setNota(rs.getInt("nota"));
+                lista.add(cursada);
+            }
+            ps.close();
+        } catch (SQLException ex) 
+        {            
+             System.out.println("Error al buscar al alumno con la id ingresada: ");            
+        }
+        if (lista.isEmpty()) 
+        {
+            System.out.println("La base de datos se encuentra vacia");      
+        }
+        return lista;
+    }
+    
+    public List<Materia> obtenerMateriasCursadas(int id_alumno) {
+        List<Materia> lista = new ArrayList<>();
+        Materia m;
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM inscripcion, materia WHERE incripcion.id_materia = materia.id_materia and inscripcion.id_alumno = ?");
+                   
+            ps.setInt(1, id_alumno);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                m = new Materia();
+                m.setId_materia(rs.getInt("id_materia"));
+                m.setNombre(rs.getString("nombre"));
+                m.setAnio(rs.getInt("anio"));
+                m.setEstado(rs.getBoolean("estado"));
+                lista.add(m);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            
+            System.out.println("Error al buscar al alumno con la id ingresada: " );
+            
+        }
+        if (lista.isEmpty()) {
+           System.out.println("La base de datos se encuentra vacia");
+           
+        }
+        return lista;
+    }
+    public List<Materia> obtenerMateriasNOCursadas(int id_materia) {
+        List<Materia> noCursada = new ArrayList<>();
+        Materia m;
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM materia WHERE id_materia NOT IN(SELECT materia.id_materia FROM materia, inscripcion WHERE materia.id_materia = inscripcion.id_materia AND inscripcion.id_alumno = ?);");
+            ps.setInt(1, id_materia);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                m = new Materia();
+                m.setId_materia(rs.getInt("id_materia"));
+                m.setNombre(rs.getString("nombre_materia"));
+                m.setAnio(rs.getInt("año"));
+                m.setEstado(rs.getBoolean("estado"));
+                noCursada.add(m);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al consultar la tabla: " );
+            
+        }
+        if (noCursada.isEmpty()) {
+            
+             System.out.println("La lista esta vacia." );
+            
+        }
+        return noCursada;
+    }
+    
+    public void borrarInscripcionMateriaAlumno(int id_alumno, int id_materia) {
+        try {
+            PreparedStatement ps = con.prepareStatement("DELETE FROM inscripcion WHERE id_alumno = ? AND id_materia = ?", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id_alumno);
+            ps.setInt(2, id_materia);
+            if (ps.executeUpdate() == 1) {
+                System.out.println("La nota del alumno ha sido borrada correctamente" );
+                
+            } else {
+                System.out.println("La nota del alumno no ha sido borrada." );
+                
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar la nota del alumno: ");
+            
+        }
+    }
+    
+    public void actualizarNota(int id_alumno, int id_materia, double nota) {
+        try {
+            PreparedStatement ps = con.prepareStatement("UPDATE inscripcion SET nota = ? WHERE id_alumno = ? AND id_materia = ?", Statement.RETURN_GENERATED_KEYS);
+            ps.setDouble(1, nota);
+            ps.setInt(2, id_alumno);
+            ps.setInt(3, id_materia);
+            if (ps.executeUpdate() == 1) {
+                System.out.println("La nota del alumno ha sido actualizada correctamente.");
+                
+            } else {
+                System.out.println("La nota del alumno ha podido ser actualizada correctamente.");
+                
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al actualizar la nota de un alumno: ");
+          
+        }
     }
     
 }
